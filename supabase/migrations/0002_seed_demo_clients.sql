9b95c7d4deb946abe92e714cef67cb654c612cb9
Skyhan <skayhan0@gmail.com>
2026-09-03 22:17:27 +0200

    feat: seed 3 synthetic demo clients

---
+++ supabase/migrations/0002_seed_demo_clients.sql
@@ -0,0 +1,13 @@
+INSERT INTO demo_clients (display_name, care_plan_summary) VALUES
+(
+  'Mevrouw De Vries',
+  'Ochtendzorg: hulp bij wassen en aankleden, medicatieherinnering en ontbijtvoorbereiding.'
+),
+(
+  'Meneer Bakker',
+  'Avondzorg: hulp bij aankleden voor de nacht, medicatie toedienen, controle bloeddruk.'
+),
+(
+  'Mevrouw Jansen',
+  'Middagzorg: wondverzorging been, hulp bij lopen naar woonkamer, korte wandeling indien mogelijk.'
+);
